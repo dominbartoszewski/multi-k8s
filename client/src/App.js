7f3b05d5b95172650413a3a1b92f8e95e2e09d59
dominikbartoszewski <dominbartoszewski@gmail.com>
2024-01-27 17:43:20 +0100

    love

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,14 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p></p>
+          <p>Kocham Agunie</p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Kocham Agunię
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
